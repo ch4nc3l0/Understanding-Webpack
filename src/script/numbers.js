3963deb03825804dfb4368405a37629272b283e1
chance <ch4nc3l0@gmail.com>
2019-03-10 20:25:21 -0400

    Add: Javascript logic for addition form

---
+++ src/script/numbers.js
@@ -0,0 +1,29 @@
+function getnumbers() {
+  // Use jQuery to get form input values
+  number1 = $("#number1")
+    .val()
+    .trim();
+  number2 = $("#number2")
+    .val()
+    .trim();
+  // Convert input strings to actual numbers
+  num1 = Number(number1);
+  num2 = Number(number2);
+  // Quick check to make sure inputs are numbers
+  if (num1 && num2) {
+    return num1, num2;
+  }
+  // Show error message if both inputs are not numbers
+  else {
+    $("#additionerror").show();
+  }
+}
+
+function addnumbers() {
+  // Run getnumbers to get inputs
+  getnumbers();
+  // Do addition
+  result = num1 + num2;
+  // Set value into HTML id named answer
+  $("#answer").val(result);
+}
