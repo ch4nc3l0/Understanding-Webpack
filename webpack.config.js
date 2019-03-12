cb6561f2e39fcfa3eee7e740683af1420781fc05
chance <ch4nc3l0@gmail.com>
2019-03-12 07:26:55 -0400

    Add: Webpack mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     entry: './src/script/index.',
+    mode: 'development',
     output: {
       path: path.resolve(__dirname, 'webpackoutput'),
       filename: 'outputfile.js'
