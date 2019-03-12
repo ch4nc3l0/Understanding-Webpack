fc845388a124034d034451220b52156a7c2fd2e4
chance <ch4nc3l0@gmail.com>
2019-03-12 14:59:39 -0400

    Add: Webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,27 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
-      js:'./src/script/index.js',
+      appjs:'./src/script/index.js',
+      bootjs:'./src/script/libs/bootstrap.bundle.min.js',
       sass:'./src/styles/index.sass'
     },
     mode: 'development',
     output: {
-      filename: "[name].js",
+      filename: "[name].[contentHash].js",
       path: path.resolve(__dirname, "webpackoutput")
     },
-
+    plugins:[
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+       }),
+    ],
     module: {
       rules: [{ 
         test: /\.sass$/,
